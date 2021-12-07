367887d6bc672d1f676ddc2b22777f9defeb1d99
parker <88166453+paarrkkeer@users.noreply.github.com>
2021-12-06 22:02:02 -0500

    Create EASTBLUEMAPLOAD.lua

---
+++ EASTBLUEMAPLOAD.lua
@@ -0,0 +1,24 @@
+local start = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2375, 28, -1585)
+wait(3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3401, 367, -2174)
+wait(3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1469, 209, 2688)
+wait(3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1125, 215, 2666)
+wait(3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2327, 108, -894)
+wait(3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2211, 60, -1810)
+wait(3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-666, 50, -3380)
+wait(3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1157, 52, -4525)
+wait(3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1913, 55, -5230)
+wait(3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(405, 137, 8860)
+wait(3)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = start
